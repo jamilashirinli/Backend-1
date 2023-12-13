0a76d087e7d9f032998980048b17712762e10bd9
jamilashirinli <camilashirinli@gmail.com>
2023-12-13 15:48:16 +0400

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const fs = require('fs');
 const userData = fs.readFileSync('./data.json');
-const users = JSON.parse(userData);
+
 // const total = users.reduce((sum, user) => sum + user.age, 0) / users.length
 // fs.writeFileSync('result.txt', `orta yas - ${total.toFixed(0)}`)
+
+
+
+
+
 const express = require('express');
 const app = express();
-
+const users = [
+    {id:1 ,name:'pen', cost:'5$', size: 'full'},
+    {id:2 ,name:'bag', cost:'10$' ,size:'full'},
+    {id:3,name:'rules', cost:'15$'},
+    {id:4,name:'book', cost:'20$'},
+    {name:'coppybook', cost:'25$'},
+    {name:'coppybook', cost:'25$'},
+    {name:'coppybook', cost:'25$'},
+    {name:'coppybook', cost:'25$'},
+    {name:'coppybook', cost:'25$'},
+    {name:'coppybook', cost:'25$'},
+]
 app.get('/', (req,res)=>{
-    res.json(users)
+
+    res.send("home page")
 })
 
-app.listen(4000, ()=>{
-    console.log("server calisdi");
+app.get('/users', (req,res)=>{
+   
+    res.send(users)
 })
+app.get('/users/id', (req,res)=>{
+    const userId = req.params.id;
+    const selectedUser = users.find(user=>user.id == userId)
+    // console.log(username); 
+    if(selectedUser){
+
+        res.send(selectedUser)
+    }else{
+        res.status(404).send('istifadeci yoxdur')
+    }
+})
+
+app.listen(5000, ()=>{
+    console.log("server quruldu");
+})
+  
